db8133019b2f0a9b85a22b1ebe2c7fe16e2973f2
Konstantin8105 <konstantin8105@gmail.com>
2018-03-19 22:38:36 +0300

    key Port

--- ssh_key_port.go
+++ ssh_key_port.go
@@ -1,20 +1,32 @@
 package ssh_config
 
+import (
+	"strconv"
+	"strings"
+)
+
 // Port    Specifies the port number to connect on the remote host.  The
 //         default is 22.
 //
 
 func init() {
 	funcInit := func() (res string) {
-		// TODO
-		panic("Not implemented")
-		return
+		return "22"
 	}
 
 	funcValid := func(value string) (res bool) {
-		// TODO
-		panic("Not implemented")
-		return
+		value = strings.TrimSpace(value)
+		port, err := strconv.Atoi(value)
+		if err != nil {
+			return false
+		}
+		if port < 0 {
+			return false
+		}
+		if port > 65535 {
+			return false
+		}
+		return true
 	}
 
 	funcParse := func(input string) (values []string, err error) {
--- ssh_key_port_test.go
+++ ssh_key_port_test.go
@@ -1,6 +1,9 @@
 package ssh_config
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 // Port    Specifies the port number to connect on the remote host.  The
 //         default is 22.
@@ -20,8 +23,10 @@ func TestPortInitFunc(t *testing.T) {
 		t.Errorf("Init-function is nil")
 	}
 	if f() == "" {
-		// TODO
 		t.Errorf("Not acceptable empty init value")
+	}
+	if f() != "22" {
+		t.Errorf("Not acceptable post number : %v", f())
 	}
 }
 
@@ -38,9 +43,20 @@ func TestPortValidFunc(t *testing.T) {
 	if f == nil {
 		t.Errorf("Valid-function is nil")
 	}
-	if !f("") {
-		// TODO
-		t.Errorf("Not acceptable empty valid-value")
+	if !f(mapInit[Port]()) {
+		t.Errorf("Not acceptable valid-value for init-value")
+	}
+	for i := 0; i < 65535+1; i++ {
+		port := fmt.Sprintf(" %d    ", i)
+		if !f(port) {
+			t.Errorf("Error for valid-value : %s", port)
+		}
+	}
+	if f(" -1   ") {
+		t.Errorf("Error for NOT valid-value : -1")
+	}
+	if f("65536 ") {
+		t.Errorf("Error for NOT valid-value : 65536")
 	}
 }
 
